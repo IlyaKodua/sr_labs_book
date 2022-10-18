38aa10f8f938e69bd7c669cf5798370f1a92597b
Kodua Ilya <ilya.kodua@mail.ru>
2022-10-18 18:23:00 +0300

    lab2

--- lab2/exercises_blank.py
+++ lab2/exercises_blank.py
@@ -13,10 +13,14 @@ def load_vad_markup(path_to_rttm, signal, fs):
     
     vad_markup = np.zeros(len(signal)).astype('float32')
         
-    ###########################################################
-    # Here is your code
-    
-    ###########################################################
+    with open(path_to_rttm) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        symbols = line.split(" ")
+        start = int(float(symbols[3])*fs)
+        stop = int(float(symbols[4])*fs)
+        vad_markup[start:start + stop] = 1
     
     return vad_markup
 
@@ -24,12 +28,30 @@ def framing(signal, window=320, shift=160):
     # Function to create frames from signal
     
     shape   = (int((signal.shape[0] - window)/shift + 1), window)
-    frames  = np.zeros().astype('float32')
+    frames  = np.zeros(shape).astype('float32')
 
-    ###########################################################
-    # Here is your code
-    
-    ###########################################################
+    signal_length  = signal.shape[0]
+    frame_length = window
+    frame_step = shift
+    num_frames = int(
+        np.ceil(float(np.abs(signal_length - frame_length)) / frame_step)) # make sure that we have at least 1 frame
+
+    pad_signal_length = num_frames * frame_step + frame_length
+    z = np.zeros((pad_signal_length - signal_length))
+    pad_signal = np.append(signal, z) # pad Signal to make sure that all frames have equal number of samples without
+                                                 # truncating any samples from the original signal
+
+    window = np.hamming(frame_length)
+    frames = np.zeros((num_frames, frame_length))
+    id_start = 0
+    id_end = frame_length
+    cnt = 0
+
+    while(id_end < len(pad_signal)):
+        frames[cnt] = pad_signal[id_start:id_end] * window
+        cnt += 1
+        id_start += frame_step
+        id_end += frame_step
     
     return frames
 
@@ -38,17 +60,14 @@ def frame_energy(frames):
     
     E = np.zeros(frames.shape[0]).astype('float32')
 
-    ###########################################################
-    # Here is your code
-    
-    ###########################################################
+    E  = np.sum(frames, axis = 1)
     
     return E
 
 def norm_energy(E):
     # Function to normalize energy by mean energy and energy standard deviation
     
-    E_norm = np.zeros(len(E)).astype('float32')
+    E_norm = (E - np.mean(E)) / np.std(E)
 
     ###########################################################
     # Here is your code
@@ -65,21 +84,22 @@ def gmm_train(E, gauss_pdf, n_realignment):
     m     = np.array([-1.00, 0.00, 1.00])
     sigma = np.array([ 1.00, 1.00, 1.00])
 
+    count_frames = len(E)
     g = np.zeros([len(E), len(w)])
     for n in range(n_realignment):
-
+        ...
         # E-step
         ###########################################################
-        # Here is your code
-
+        eStep_get_g_i = lambda e: (w * gauss_pdf(e, m, sigma)) / np.sum((w * gauss_pdf(e, m, sigma)))
+        g = np.array(list(map(eStep_get_g_i, E)))
         ###########################################################
 
         # M-step
         ###########################################################
-        # Here is your code
-
-        ###########################################################
-        
+        w = g.sum(axis=0) / count_frames
+        m = (g*np.repeat([E], 3, axis=0).T).sum(axis=0) / (count_frames * w)
+        sigma = np.array([(g.T[idx]*(E - m[idx])**2).sum() / (count_frames * w[idx]) for idx in range(3)])
+        sigma = np.sqrt(sigma)
     return w, m, sigma
 
 def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
@@ -87,10 +107,8 @@ def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
 
     g0 = np.zeros(len(E))
 
-    ###########################################################
-    # Here is your code
-
-    ###########################################################
+    g0 = np.array([(w[0]*gauss_pdf(E[idx], m[0], sigma[0])) / (w*gauss_pdf(E[idx], m, sigma)).sum()
+                   for idx in range(len(E))])
             
     return g0
 
@@ -133,23 +151,14 @@ def energy_gmm_vad(signal, window, shift, gauss_pdf, n_realignment, vad_thr, mas
 def reverb(signal, impulse_response):
     # Function to create reverberation effect
     
-    signal_reverb = np.zeros(len(signal)).astype('float32')
     
-    ###########################################################
-    # Here is your code
-    
-    ###########################################################
-    
-    return signal_reverb
+    return np.convolve(signal,impulse_response, 'same')
 
 def awgn(signal, sigma_noise):
     # Function to add white gaussian noise to signal
     
     signal_noise = np.zeros(len(signal)).astype('float32')
     
-    ###########################################################
-    # Here is your code
-    
-    ###########################################################
+    signal_noise = signal + np.random.normal(0, sigma_noise, len(signal))
     
     return signal_noise
