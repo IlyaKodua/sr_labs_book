67355edb23c4815846b522f72523e43ecb03fd33
Kodua Ilya <ilya.kodua@mail.ru>
2022-12-23 18:46:20 +0300

    i need it...

--- lab3/LossFunction.py
+++ lab3/LossFunction.py
@@ -158,12 +158,31 @@ class Angleproto(nn.Module):
 
         print('Initialised AngleProto')
 
+    # def forward(self, x, label=None):
+
+    #     assert x.size()[1] >= 2
+
+    #     out_anchor      = torch.mean(x[:,1:,:],1)
+    #     out_positive    = x[:,0,:]
+    #     stepsize        = out_anchor.size()[0]
+
+    #     cos_sim_matrix  = F.cosine_similarity(out_positive.unsqueeze(-1),out_anchor.unsqueeze(-1).transpose(0,2))
+    #     torch.clamp(self.w, 1e-6)
+    #     cos_sim_matrix = cos_sim_matrix * self.w + self.b
+        
+    #     label   = torch.from_numpy(numpy.asarray(range(0,stepsize))).cuda()
+    #     nloss   = self.criterion(cos_sim_matrix, label)
+    #     prec1   = accuracy(cos_sim_matrix.detach(), label.detach(), topk=(1,))[0]
+
+    #     return nloss, prec1
+
+
+
     def forward(self, x, label=None):
 
-        print(x.shape)
         assert x.size()[1] >= 2
 
-        out_anchor      = torch.mean(x[:,1],1)
+        out_anchor      = torch.mean(x[:,1:],1)
         out_positive    = x[:,0]
         stepsize        = out_anchor.size()[0]
 
