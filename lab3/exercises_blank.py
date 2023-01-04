92ea5187f514c33850c02d8e9b78ad6dfd3bc03d
Kodua Ilya <ilya.kodua@mail.ru>
2023-01-04 14:51:52 +0300

    [-] remove print

--- lab3/exercises_blank.py
+++ lab3/exercises_blank.py
@@ -264,9 +264,6 @@ class MainModel(nn.Module):
         else:
             
             outp = outp.reshape(2, -1, outp.size()[-1]).transpose(1, 0).squeeze(1)
-            print(data.shape)
-            print(label.shape)
-            print(outp.shape)
             nloss, prec1 = self.__L__.forward(outp, label)
 
             return nloss, prec1
